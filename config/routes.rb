f64cfb85ee16a1e9775fd970af2981aebbdad644
Reece_Collett <rcollett@spartaglobal.com>
2018-08-28 17:43:00 +0100

    All tests complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,35 @@
 Rails.application.routes.draw do
 
-  get 'books/index'
+  # get 'books/index'
+  #
+  # get 'books/show'
+  #
+  # get 'books/new'
+  #
+  # post 'books/create'
+  #
+  # delete 'books/destroy'
+  #
+  # get 'books/edit'
+  #
+  # put 'books/update'
+  #
+  get '/', to: "books#index"
 
-  get 'books/show'
+  get '/books/new', to: "books#new"
 
-  get 'books/new'
+  post '/books', to: "books#create"
 
-  get 'books/create'
+  get '/books/show', to: "books#show"
 
-  get 'books/destroy'
+  get '/books/edit', to: "books#edit"
 
-  get 'books/edit'
+  put '/books/update', to: "books#update"
 
-  get 'books/update'
-
-  get 'books', to: "books#index"
-
-  get 'books/new', to: "books#new"
-
-  post 'books', to: "books#create"
-
-  get 'books/:id', to: "books#show"
-
-  get 'books/:id/edit', to: "books#edit"
-
-  put 'books/:id', to: "books#update"
-
-  delete 'books/:id', to: "books#destroy"
+  delete '/books/destroy', to: "books#destroy"
 
   # BONUS
-  # resources :books
+  resources :books
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
